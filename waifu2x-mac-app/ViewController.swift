debb4700894dc28dc3392e046029f09167bb5351
kgriesbach <kgriesbach@users.noreply.github.com>
2018-04-20 12:35:04 +0200

    Update ViewController.swift

--- waifu2x-mac-app/ViewController.swift
+++ waifu2x-mac-app/ViewController.swift
@@ -10,83 +10,70 @@ import Cocoa
 import waifu2x_mac
 
 class ViewController: NSViewController {
-
-    @IBOutlet weak var inImg: NSImageView!
+    
     @IBOutlet weak var outImg: NSImageView!
-    @IBOutlet weak var pickBtn: NSButton!
     @IBOutlet weak var processBtn: NSButton!
-    @IBOutlet weak var saveBtn: NSButton!
     @IBOutlet weak var status: NSTextField!
+    @IBOutlet weak var pathInput: NSTextField!
+    
+    var urls: [URL]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     override func viewDidAppear() {
         super.viewDidAppear()
         view.window?.styleMask.remove(.resizable)
+        
+        self.pathInput.stringValue = "/Users/kevin/Downloads/Pictures/"
     }
     
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-    @IBAction func pickImage(_ sender: Any) {
-        let dialog = NSOpenPanel()
-        dialog.allowsMultipleSelection = false
-        dialog.canChooseDirectories = false
-        dialog.allowedFileTypes = ["public.image"]
-        dialog.beginSheetModal(for: view.window!) { (resp) in
-            if resp == NSApplication.ModalResponse.OK {
-                let url = dialog.urls[0]
-                self.inImg.image = NSImage(contentsOf: url)
-            }
+            // Update the view, if already loaded.
         }
     }
     
     @IBAction func processImage(_ sender: Any) {
+        guard self.pathInput.stringValue.count > 0 else { return }
+        let url = URL(fileURLWithPath: self.pathInput.stringValue)
+        let fileMan = FileManager.default
+        
+        guard let tempUrls = try? fileMan.contentsOfDirectory(at: url,
+                                                              includingPropertiesForKeys: nil,
+                                                              options: FileManager.DirectoryEnumerationOptions.skipsHiddenFiles) else { return }
+        self.urls = tempUrls
+        
         let background = DispatchQueue(label: "background")
-        guard let img = inImg.image else {
-            return
-        }
-        pickBtn.isEnabled = false
+        
         processBtn.isEnabled = false
-        saveBtn.isEnabled = false
-        let start = DispatchTime.now().uptimeNanoseconds
-        background.async {
-            guard let outImage = Waifu2x.run(img, model: Model.anime_noise2_scale2x) else {
-                return
-            }
-            DispatchQueue.main.async {
-                self.outImg.image = outImage
-                debugPrint("\(outImage.size)")
-                self.pickBtn.isEnabled = true
-                self.processBtn.isEnabled = true
-                self.saveBtn.isEnabled = true
-                let end = DispatchTime.now().uptimeNanoseconds
-                self.status.stringValue = "Time elapsed: \(Float(end - start) / 1_000_000_000)"
-            }
-        }
-    }
-    
-    @IBAction func saveImage(_ sender: Any) {
-        guard let oImg = outImg.image else {
-            return
-        }
-        let dialog = NSSavePanel()
-        dialog.allowedFileTypes = ["public.png"]
-        dialog.beginSheetModal(for: view.window!) { (resp) in
-            if resp == NSApplication.ModalResponse.OK {
-                guard let url = dialog.url else {
+        for jj in 0..<self.urls!.count {
+            let start = DispatchTime.now().uptimeNanoseconds
+            background.async {
+                let url = self.urls![jj]
+                var img = NSImage(contentsOf: url)
+                guard let outImage = Waifu2x.run(img, model: Model.anime_noise2_scale2x) else {
                     return
                 }
-                oImg.pngWrite(to: url)
+                img = nil
+                DispatchQueue.main.async {
+                    self.outImg.image = outImage
+                    debugPrint("jj = ", jj)
+                    debugPrint("size = \(outImage.size)")
+                    let end = DispatchTime.now().uptimeNanoseconds
+                    self.status.stringValue = "Time elapsed: \(Float(end - start) / 1_000_000_000)"
+                    let path = self.urls![jj].appendingPathExtension("_2x.png")
+                    debugPrint("path = ", path.absoluteString)
+                    debugPrint("write = ", outImage.pngWrite(to: path))
+                    
+                    if jj == (self.urls!.count - 1) {
+                        self.processBtn.isEnabled = true
+                    }
+                }
             }
         }
     }
-    
 }
 
